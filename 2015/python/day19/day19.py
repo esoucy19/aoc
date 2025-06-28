cb171e0ca1f04e2e182b9a8d27a9086e87d9da97
Etienne Soucy <esoucy@myself.com>
2025-06-28 10:38:41 -0400

    old uncommited changes

--- 2015/python/day19/day19.py
+++ 2015/python/day19/day19.py
@@ -1,8 +1,84 @@
-from collections.abc import Sequence
+import typing
+import sys
+from pathlib import Path
+
+import deal
 
 
-def make_new_molecules(
-    molecule: str, replacements: Sequence[tuple[str, str]]
-) -> set[str]:
-    """Return all molecules from a molecule and replacements."""
-    return set()
+@deal.inv(lambda rep: len(rep.frm) > 0)
+@deal.inv(lambda rep: len(rep.to) > 0)
+class Rep(typing.NamedTuple):
+    frm: str
+    to: str
+
+
+@deal.inv(lambda mol: len(mol) > 0)
+class Mol(str):
+    pass
+
+
+@deal.ensure(lambda mol, rep, result: len(result) == mol.count(rep.frm))
+@deal.ensure(lambda mol, rep, result: (rep.frm not in mol) or len(result) > 0)
+@deal.ensure(
+    lambda mol, rep, result: all(
+        mol[idx : idx + len(rep.frm)] == rep.frm for idx in result
+    )
+)
+def get_frm_indexes(mol: Mol, rep: Rep) -> tuple[int, ...]:
+    idxs = []
+    idx = -1
+    for _ in range(mol.count(rep.frm)):
+        idx = mol.index(rep.frm, idx + 1)
+        idxs.append(idx)
+    return tuple(idxs)
+
+
+# If replacement is bigger than original, then new molecule is bigger than
+# original.
+@deal.ensure(
+    lambda _: (not len(_.rep.to) > len(_.rep.frm)) or len(_.result) > len(_.mol)
+)
+# If replacement is smaller than original, then new molecule is smaller than
+# original.
+@deal.ensure(
+    lambda _: (not len(_.rep.to) < len(_.rep.frm)) or len(_.result) < len(_.mol)
+)
+@deal.ensure(lambda _: _.result[_.idx : _.idx + len(_.rep.to)] == _.rep.to)
+def get_new_molecule(mol: Mol, rep: Rep, idx: int) -> Mol:
+    head = mol[:idx]
+    tail = mol[idx + len(rep.frm) :]
+    return Mol(f"{head}{rep.to}{tail}")
+
+
+def get_new_molecules(
+    mol: Mol, rep: Rep, idxs: tuple[int, ...]
+) -> frozenset[Mol]:
+    return frozenset(get_new_molecule(mol, rep, idx) for idx in idxs)
+
+
+@deal.ensure(lambda mol, rep, result: (rep.frm not in mol) or len(result) > 0)
+def generate_new_molecules(mol: Mol, rep: Rep) -> frozenset[Mol]:
+    return get_new_molecules(mol, rep, get_frm_indexes(mol, rep))
+
+
+def decode_rep(line: str) -> Rep | None:
+    print(line)
+    frm, to = map(str.strip, line.split("=>"))
+    return Rep(frm, to)
+
+
+def main() -> None:
+    pth = sys.argv[1]
+    with Path(pth).open("r", encoding="utf8") as fp:
+        lines = list(filter(None, fp.read().splitlines()))
+        lines.reverse()
+    mol = lines[0]
+    reps = filter(None, tuple(decode_rep(line) for line in lines[1:]))
+    mols = set()
+    for rep in reps:
+        mols.update(generate_new_molecules(mol, rep))
+    print(len(mols))
+
+
+if __name__ == "__main__":
+    main()
--- 2015/python/day19/day19_test.py
+++ 2015/python/day19/day19_test.py
@@ -1,10 +1,27 @@
+import deal
+
 import day19
 
 
-def test_hohoho() -> None:
-    replacements = (("H", "HO"), ("H", "OH"), ("O", "HH"))
-    input_string = "HOH"
+def test_get_from_indexes():
+    mol = day19.Mol("HOHOHO")
+    rep = day19.Rep("H", "HO")
+    idxs = day19.get_frm_indexes(mol, rep)
+    assert idxs == (0, 2, 4)
 
-    expected = frozenset(("HOOH", "HOHO", "OHOH", "HOOH", "HHHH"))
-    received = frozenset(day19.make_new_molecules
-    assert received == expected
+
+@deal.cases(day19.get_frm_indexes)
+def test_get_from_indexes_cases(case):
+    case()
+
+
+def test_generate_new_molecules():
+    mol = day19.Mol("HOH")
+    rep = day19.Rep("H", "HO")
+    new_mols = day19.generate_new_molecules(mol, rep)
+    assert new_mols == {"HOOH", "HOHO"}
+
+
+@deal.cases(day19.generate_new_molecules)
+def test_generate_new_molecules_cases(case):
+    case()
